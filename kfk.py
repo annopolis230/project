f1bd87aa2125874618adbfdb2bbf618f62f8d95e
Zane DeVore <qwertycpkio@gmail.com>
2021-10-25 20:07:12 -0400

    poop

---
+++ kfk.py
@@ -0,0 +1,5 @@
+x = '9'
+y = 9
+int(x)
+
+print(x+y)
---
+++ testing.py
@@ -0,0 +1,56 @@
+import math
+
+file = open("C:/test.txt","r")
+data = str(file.read())
+dataList = []
+readList = []
+
+num = -1
+
+for i in data:
+    dataList.append(i)
+
+x = True
+
+for i in dataList:
+    if i != ' ':
+        num = num+1
+        if i.isdigit() == False:
+            print("Input must be a number")
+            x = False
+            break
+        else:
+            print('Input Data', num +1,':', i)
+            readList.append(int(i))
+            
+
+if x == True:
+    deltaQ = readList[0] - readList [1]
+    deltaP = readList[2] - readList [3]
+    avgQ = (readList[0] + readList[1])/2
+    avgP = (readList[2] + readList [3]) / 2
+    elasticity = (deltaQ/avgQ)/(deltaP/avgP)
+    elastype = ""
+
+    if (elasticity == 1):
+        elastype = "Unitary"
+    elif (elasticity > 1) and (elasticity < 5):
+        elastype = "Relatively Elastic"
+    elif (elasticity > 5):
+        elastype = "Highly Elastic"
+    elif (elasticity <1) and (elasticity > 0.5):
+        elastype = "Relatively Inelastic"
+    elif (elasticity < 0.5):
+        elastype = "Highly Inelastic"
+
+    print("Elasticity Type: ", elastype)
+    print("Elasticity Coefficient: ", elasticity)
+
+
+    print(readList[0] + readList[1])
+
+else:
+    print("Calculation aborted")
+
+
+
